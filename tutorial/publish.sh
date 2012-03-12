64bd638ede8e4125fbe1f45061b03b4f470a3b75
Xavier Antoviaque <xavier@antoviaque.org>
2012-03-12 18:45:57 -0400

    Improved presentation & CSS, added screenshot, code highlighting

--- tutorial/publish.sh
+++ tutorial/publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-markdown_py tutorial.txt > _tutorial.html ; cat header.html _tutorial.html >tutorial.html
+markdown_py tutorial.txt > _tutorial.html 
+cat header.html _tutorial.html | sed -e 's/<code>/<code class="javascript">/' >tutorial.html
