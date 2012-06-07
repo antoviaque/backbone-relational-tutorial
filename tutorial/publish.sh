8010131d8b7c6db2ff3360e9e8ad40d0e790b961
Xavier Antoviaque <xavier@antoviaque.org>
2012-06-07 12:15:21 -0300

    Fixes #2 - Typo (+revert Typoto python-markdown for generation)

--- tutorial/publish.sh
+++ tutorial/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-md2html tutorial.txt > _tutorial.html 
+markdown_py -x toc tutorial.txt > _tutorial.html 
 cat header.html _tutorial.html | sed -e 's/<code>/<code class="javascript">/' >tutorial.html
 rm _tutorial.html
